f8b26730695821fe04326246089e130c6475e7dc
starfactory <johzangerle@tsn.at>
2025-09-30 19:13:18 +0200

    test für API link

--- WebAPI.py
+++ WebAPI.py
@@ -1,4 +1,12 @@
 import requests
 
-url = "https://api.example.com/data"  # Dein API-Link
+url = "https://api.geckoterminal.com/api/v2/networks" 
 response = requests.get(url)
+
+print("Status Code:", response.status_code)
+
+if response.status_code == 200:
+    data = response.json()   # JSON in Python-Daten umwandeln
+    print(data)              # Ausgabe der API-Antwort
+else:
+    print("Fehler:", response.status_code)
