77c8f209db1d7b7f36048e7a1413f0ba682dba52
starfactory <johzangerle@tsn.at>
2025-10-01 11:04:25 +0200

    ausweitung links

--- WebAPI.py
+++ WebAPI.py
@@ -1,12 +1,16 @@
 import requests
 
-url = "https://api.geckoterminal.com/api/v2/networks" 
-response = requests.get(url)
+
+#Dokumentation: https://docs.coingecko.com/docs/sdk
+url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin&x_cg_demo_api_key=CG-kGgB4jzBXM8qNe4is6PitM4f"
+web_url = "https://api.geckoterminal.com/api/v2/networks"
+response = requests.get(web_url)
 
 print("Status Code:", response.status_code)
 
 if response.status_code == 200:
     data = response.json()   # JSON in Python-Daten umwandeln
-    print(data)              # Ausgabe der API-Antwort
+    print(data)              
 else:
     print("Fehler:", response.status_code)
+    
